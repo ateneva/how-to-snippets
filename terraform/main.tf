369c194bb521eaba61285bd454d3b42c2525b6b4
ateneva <tenevaa21@gmail.com>
2025-10-03 16:50:26 +0200

    minor: add-gcp-terraform-buckets-and-datasets-examples

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,37 @@ provider "google" {
   zone    = "europe-west1-b"
 }
 
-resource "google_storage_bucket" "gcp_geeking" {
-  name     = "gcp_geeking"
-  location = "EU"
+resource "google_storage_bucket" "eu-data-challenge" {
+  name          = "eu-data-challenge"
+  location      = "EU"
+  storage_class = "STANDARD"
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+  force_destroy = true
+
+  retention_policy {
+    retention_period = 2592000
+  }
+}
+
+resource "google_storage_bucket" "eu-data-books" {
+  name          = "eu-data-books"
+  location      = "europe-west1"
+  storage_class = "ARCHIVE"
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "the-data-challenge" {
+  dataset_id                  = "the_data_challenge"
+  friendly_name               = "the-data-challenge"
+  description                 = "This dataset stores the data-engineering challenge"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
 }
